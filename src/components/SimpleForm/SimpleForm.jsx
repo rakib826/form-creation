17e9ebd79e06ce69130d00b5ee23ae0c941f01fc
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-05 01:00:06 +0600

    created handle submit

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,20 @@
-import React from "react";
 
 const SimpleForm = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log('Form submitted');
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>1{" "}
+      <form onSubmit={handleSubmit} >
+        <input className="border-2 mb-2 p-1 rounded w-2/6" type="text" name="name" /> <br />
+        <input className="border-2 mb-2 p-1 rounded w-2/6" type="text" name="email" /> <br />
+        <input className="border-2 py-1 px-4 rounded-md bg-green-600 text-white" type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
